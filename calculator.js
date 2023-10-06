391f325fc72c8d349db45666f54847cdfac9746a
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-10-06 16:43:39 +1100

    Link buttons in index.html to calculator.js logic

--- calculator.js
+++ calculator.js
@@ -11,20 +11,42 @@ function divide(a, b) {
     return a / b;
 }
 
-let a, b, operator;
-
 function operate(a, operator, b) {
     switch (operator) {
-        case "+":
+        case '+':
             return add(a,b);
             break;
-        case "-":
+        case '-':
             return subtract(a,b);
             break;
-        case "x":
+        case 'x':
             return multiply(a,b);
             break;
-        case "/":
+        case '/':
             return divide(a,b);
     }
 }
+
+let display = '';
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    if (button.id === 'clear') {
+        button.addEventListener('click', () => {
+            display = '';
+            const displayDiv = document.getElementById('display');
+            displayDiv.textContent = display;
+            console.log(display);
+        });
+    } else {
+        button.addEventListener('click', () => {
+            display += button.textContent;
+            const displayDiv = document.getElementById('display');
+            displayDiv.textContent = display;
+            console.log(display);
+        });
+    }
+});
+
+
+// b, operator, display =
