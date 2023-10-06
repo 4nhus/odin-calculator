f3da6f0ca5d5acdc92410b9eed29819fc805a7b0
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-10-06 17:13:25 +1100

    Add button logic to correctly update display

--- calculator.js
+++ calculator.js
@@ -1,12 +1,15 @@
 function add(a, b) {
     return a + b;
 }
+
 function subtract(a, b) {
     return a - b;
 }
+
 function multiply(a, b) {
     return a * b;
 }
+
 function divide(a, b) {
     return a / b;
 }
@@ -14,36 +17,55 @@ function divide(a, b) {
 function operate(a, operator, b) {
     switch (operator) {
         case '+':
-            return add(a,b);
+            return add(a, b);
             break;
         case '-':
-            return subtract(a,b);
+            return subtract(a, b);
             break;
         case 'x':
-            return multiply(a,b);
+            return multiply(a, b);
             break;
         case '/':
-            return divide(a,b);
+            return divide(a, b);
     }
 }
 
+function calculate(display) {
+
+}
+
 let display = '';
+const displayDiv = document.getElementById('display');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
-    if (button.id === 'clear') {
-        button.addEventListener('click', () => {
-            display = '';
-            const displayDiv = document.getElementById('display');
-            displayDiv.textContent = display;
-            console.log(display);
-        });
+    if (isNaN(button.id)) {
+        if (button.id === 'clear') {
+            button.addEventListener('click', () => {
+                display = '';
+                displayDiv.textContent = display;
+            });
+        } else if (button.id === '=') {
+            button.addEventListener('click', () => {
+                display = calculate(display);
+                displayDiv.textContent = display;
+            });
+        } else {
+            button.addEventListener('click', () => {
+                if (display !== '') {
+                    if (isNaN(display[display.length - 1])) {
+                        display = display.substring(0, display.length - 1).concat(button.textContent);
+                    } else {
+                        display += button.textContent;
+                    }
+                    displayDiv.textContent = display;
+                }
+            });
+        }
     } else {
         button.addEventListener('click', () => {
             display += button.textContent;
-            const displayDiv = document.getElementById('display');
             displayDiv.textContent = display;
-            console.log(display);
         });
     }
 });
